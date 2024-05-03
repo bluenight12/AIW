ffe5f2399bdc0bd34f5ffeadc52760e385c18967
bluenight12 <ekehd258@gmail.com>
2024-05-03 10:37:28 +0900

    change camera.py

--- pages/camera.py
+++ pages/camera.py
@@ -1,36 +1,38 @@
 import streamlit as st
 import cv2
 
+
 def main():
     st.set_page_config(page_title="Streamlit WebCam App")
     st.title("Webcam Display Steamlit App")
     st.caption("Powered by OpenCV, Streamlit@@@@@")
     cap = cv2.VideoCapture(0)
-    
+
     frame_placeholder = st.empty()
     col1, col2 = st.columns([1, 1])
     with col1:
-        with st.align('middle'):
-            start_button_pressed = st.button("시작")
+        start_button_pressed = st.button("시작")
     with col2:
-        stop_button_pressed = st.button("멈춰")
-    shutter_button_pressed = st.button("촬영") # 자동 촬영으로 바꾸기
+        next_button_pressed = st.button("넘어가기")
+    shutter_button_pressed = st.link_button("촬영", "/pages/voice.py")  # 자동 촬영으로 바꾸기
     if start_button_pressed:
         pass
-    while cap.isOpened() and not stop_button_pressed:
+    while cap.isOpened() and not next_button_pressed:
         ret, frame = cap.read()
         if not ret:
             st.write("Video Capture Ended")
             break
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_placeholder.image(frame,channels="RGB")
+        frame_placeholder.image(frame, channels="RGB")
         if cv2.waitKey(1):
-            if stop_button_pressed:
+            if next_button_pressed:
                 break
             elif shutter_button_pressed:
-                cv2.imwrite("Cam.jpg", frame)
+                cv2.imwrite("Cam.jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+                shutter_button_pressed = False
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
---
+++ pages/voice.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import speech_recognition as sr
+
+def recognize_and_save_text(output_file):
+    # 음성 인식기 객체 생성
+    recognizer = sr.Recognizer()
+
+    while True:
+        # 마이크로 음성 입력 받기
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source, duration=0)  # 배경 소음에 대한 자동 조절
+            print("말씀해주세요...")
+            try:
+                audio = recognizer.listen(source, timeout=1.5, phrase_time_limit=3)  # 마이크에서 음성 입력을 듣기
+            except sr.WaitTimeoutError:
+                return list("음성이 입력되지 않았습니다.")
+
+        # 음성을 텍스트로 변환
+        try:
+            text = recognizer.recognize_whisper(audio, language='ko-KR')
+            print("음성 인식 결과:", text)
+
+            # 인식된 텍스트를 text.txt 파일에 저장
+            with open(output_file, "w") as file:
+                file.write(text)
+            print("인식된 단어를 text.txt 파일에 저장했습니다.")
+            return text
+
+        except sr.UnknownValueError:
+            return list("음성을 인식할 수 없습니다. 다시 시도해주세요.")
+        except sr.RequestError as e:
+            return list("Google Speech Recognition 서비스에 접근할 수 없습니다; {0}".format(e))
+        
+def main():
+    st.title("Voice Test")
+    
