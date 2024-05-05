584619d3d3d41f054add3980edf419467f6ce542
s0066 <asd21sd@asda1>
2024-05-05 11:37:09 +0900

    change make_image.py

--- pages/make_image.py
+++ pages/make_image.py
@@ -5,7 +5,7 @@ import streamlit as st
 from refer import webui_api as api
 from util import Clothes_Segmentation as cs
 def Recent_file():
-    files = glob.glob('./refer/api_out/img2img/img2img_*.png')
+    files = glob.glob('api_out/img2img/img2img-*.png')
     recent_file = max(files, key=os.path.getmtime)
     return recent_file
 def Make_img():
@@ -21,13 +21,17 @@ def main():
     st.title("Image Test")
     text = st.session_state.get("text")
     frame_placeholder = st.empty()
-    make_button_pressed = st.button("이미지 만들기", on_click=Make_img(), args=("Hi",))
-    
+    make_button_pressed = st.button("이미지 만들기",  args=("Hi",))
+
+
+
     if make_button_pressed:
+        Make_img()
+        image = cv2.imread(Recent_file())
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        frame_placeholder.image(image, channels="RGB")
         pass
-    image = cv2.imread(Recent_file())
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    frame_placeholder.image(image, channels="RGB")
+
 
 if __name__ == "__main__":
     main()
