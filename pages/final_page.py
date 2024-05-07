3b76325d9d4b1d841171e05fd80d665297e7f6ed
bluenight12 <ekehd258@gmail.com>
2024-05-07 12:29:10 +0900

    change final page

--- pages/final_page.py
+++ pages/final_page.py
@@ -6,6 +6,13 @@ import cv2
 
 def main():
     st.markdown("""
+            <div style='text-align: left;'>
+                <a href="/" target="_self">
+                    <button style='background-color: #0068c9; color: white; padding: 10px 20px; border-radius: 5px; border: none; font-size: 16px; cursor: pointer;'>처음으로</button>
+                </a>
+            </div>
+    """, unsafe_allow_html=True)
+    st.markdown("""
         <div style='display: flex; align-items: center; justify-content: center;'>
             <div style='text-align: center;'>
                 <h1>최종 결과입니다</h1>
@@ -30,5 +37,7 @@ def main():
         st.markdown(f"<div style =' text-align:center; justify-content: center; font-size:40px; '>옷 정보 링크 >> </div>", unsafe_allow_html=True)
     with col2:
         st.image(i)
+
+    
 if __name__ == "__main__":
     main()
